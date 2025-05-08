24a545e1195a05978961867ff42bb84a558e32fc
sylvayas <yasminesylva0758@gmail.com>
2025-05-08 13:38:59 +0000

    second  commit 08mai

--- app/a-propos/mot-du-directeur/page.tsx
+++ app/a-propos/mot-du-directeur/page.tsx
@@ -17,7 +17,7 @@ export default function MotDuDirecteurPage() {
             <div className="sticky top-24">
               <div className="relative w-full aspect-square rounded-lg overflow-hidden mb-4">
                 <Image
-                  src="/images/image1.jpg"
+                  src="/images/dg.jpg"
                   alt="Dr. Amadou Diallo - Directeur du CAPEC"
                   fill
                   className="object-cover"
