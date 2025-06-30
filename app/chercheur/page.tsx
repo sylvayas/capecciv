3f999cfc385c90e26da4f73b77585c4f251a448f
sylvayas <yasminesylva0758@gmail.com>
2025-06-30 12:23:47 +0000

    four commit 30juin

--- app/chercheur/page.tsx
+++ app/chercheur/page.tsx
@@ -136,7 +136,7 @@ const seniorResearchers = [
     id: "6",
     name: "Dr Nohoua TRAORE",
     title: "Economiste Chercheur à la Cellule d'Analyse de Politiques Économiques  du  Cires (CAPEC). ",
-    photo: "/images/placeholder-icon.png",
+    photo: "/images/userimage.webp",
     expertise: ["Economie"]
   },
   {
